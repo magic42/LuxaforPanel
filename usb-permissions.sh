9af306639ce1798146ede98247cc76eee75236a3
Carl Fagg <carl.fagg@mobilefun.co.uk>
2015-12-04 12:56:53 +0000

    Added udev refresh to script + Script can now be used to fix existing permissions

--- usb-permissions.sh
+++ usb-permissions.sh
@@ -2,3 +2,4 @@ if [ ! -f /etc/udev/rules.d/10-luxafor.rules ]; then
     sudo touch /etc/udev/rules.d/10-luxafor.rules
     sudo echo "SUBSYSTEM==\"usb\", ATTRS{idProduct}==\"f372\", ATTRS{idVendor}==\"04d8\", MODE=\"666\"" >> /etc/udev/rules.d/10-luxafor.rules
 fi
+sudo udevadm trigger
